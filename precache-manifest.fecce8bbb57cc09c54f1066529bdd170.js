8a9f4941a908b605f4436a3a5946e9d2174e91f9
Kevin Samosir <bintangkevin29@gmail.com>
2020-04-04 21:02:52 +0700

    Updates

---
+++ precache-manifest.fecce8bbb57cc09c54f1066529bdd170.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a08c03475e9715c5b8997d8c27d60e3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "00cd17d18529be4b575d",
+    "url": "/monsters-rolodex/static/css/main.b781bc25.chunk.css"
+  },
+  {
+    "revision": "d064ea85da7c2e271b4d",
+    "url": "/monsters-rolodex/static/js/2.d9d4b753.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.d9d4b753.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00cd17d18529be4b575d",
+    "url": "/monsters-rolodex/static/js/main.2ad2dce7.chunk.js"
+  },
+  {
+    "revision": "660b49c12bc08bf312d8",
+    "url": "/monsters-rolodex/static/js/runtime-main.00209c30.js"
+  }
+]);
